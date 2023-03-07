29c61b822d7b67035d088ae7cc6320abf632b038
koss054 <konstantin054@abv.bg>
2023-03-07 14:49:32 +0200

    Finish rewriting game logic on new script
    Not as many features as last time. I'll be reworking the progression as well, making it a bit harder to start.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 // Variables used throughout the page.
 function pageVariables() {
   let minNumber = 1;
-  let maxNumber = 15;
+  let maxNumber = 5;
   let score = Math.floor(maxNumber / 2);
   let totalScore = 0;
-  let secretNumber = 5;
+  let secretNumber = Math.floor(Math.random() * maxNumber) + minNumber;
   let isGuessed = false;
 
   return {
@@ -17,7 +17,35 @@ function pageVariables() {
   };
 }
 
+let vars = pageVariables();
+
 // Functions used in page functions.
+function setValuesOnPage() {
+  setBetweenRange();
+  setScore();
+  setTotalScore();
+}
+
+function setBetweenRange() {
+  document.getElementById(
+    "between"
+  ).innerHTML = `(Between ${vars.minNumber} and ${vars.maxNumber})`;
+}
+
+function setScore() {
+  document.getElementById("score").innerHTML = `${vars.score}`;
+}
+
+function setTotalScore() {
+  document.getElementById("total-score").innerHTML = `${vars.totalScore.toFixed(
+    1
+  )}`;
+}
+
+function setSecretNumber() {
+  return Math.floor(Math.random() * vars.maxNumber) + vars.minNumber;
+}
+
 function resetScreen() {
   pageVariables.isGuessed = false;
   document.querySelector("body").style.background = "#222";
@@ -25,39 +53,45 @@ function resetScreen() {
   document.querySelector(".btn.again").style.display = "none";
   document.querySelector(".btn.check").style.display = "inline-block";
   document.querySelector(".number").innerHTML = "?";
-  document.querySelector(".message").innerHTML = "Start guessing...";
+  document.querySelector("#message").innerHTML = "Start guessing...";
+}
+
+function updateOnClick(userGuess, guessMessageElement) {
+  if (userGuess == vars.secretNumber) {
+    guessedNumber(vars.secretNumber);
+    guessMessageElement.innerHTML =
+      "✅ You guessed the number! Changing number...";
+    vars.totalScore += vars.score;
+    vars.secretNumber = setSecretNumber();
+    vars.isGuessed = true;
+  } else if (userGuess > vars.secretNumber) {
+    guessMessageElement.innerHTML = "☝ Too high...";
+    vars.score--;
+  } else if (userGuess < vars.secretNumber) {
+    guessMessageElement.innerHTML = "👇 Too low...";
+    vars.score--;
+  }
+}
+
+function guessedNumber(secretNumber) {
+  document.querySelector("body").style.background = "#60b347";
+  document.querySelector(".number").style.width = "30rem";
+  document.querySelector(".btn.check").style.display = "none";
+  document.querySelector(".btn.again").style.display = "inline-block";
+  document.querySelector(".number").innerHTML = `${secretNumber}`;
+}
+
+function outOfScore() {
+  document.querySelector("body").style.background = "#ab263a";
+  document.querySelector(".btn.check").style.display = "none";
+  document.querySelector(".btn.again").style.display = "inline-block";
+  document.querySelector(".number").innerHTML = "X";
 }
 
 // Page functions.
 const initialPageState = function () {
-  // Variables that set the initial values on the page.
-  const variables = pageVariables();
-
   // Function that sets all initial values.
-  setValuesOnPage();
-
-  // Function definitions.
-  function setValuesOnPage() {
-    setBetweenRange();
-    setScore();
-    setTotalScore();
-
-    function setBetweenRange() {
-      document.getElementById(
-        "between"
-      ).innerHTML = `(Between ${variables.minNumber} and ${variables.maxNumber})`;
-    }
-
-    function setScore() {
-      document.getElementById("score").innerHTML = `${variables.score}`;
-    }
-
-    function setTotalScore() {
-      document.getElementById(
-        "total-score"
-      ).innerHTML = `${variables.totalScore.toFixed(1)}`;
-    }
-  }
+  setValuesOnPage(vars.minNumber, vars.maxNumber, vars.score, vars.totalScore);
 };
 
 const buttonEvents = function () {
@@ -70,12 +104,39 @@ const buttonEvents = function () {
     againClicked();
   });
 
+  btnCheck.addEventListener("click", function () {
+    checkClicked();
+  });
+
   // Inner functions for buttons.
   function againClicked() {
-    pageVariables.score = Math.floor(pageVariables.maxNumber);
+    vars.score = Math.floor(vars.maxNumber / 2);
     resetScreen();
   }
+
+  function checkClicked() {
+    const userGuess = document.getElementById("guess").value;
+    const guessMessage = document.getElementById("message");
+
+    if (!userGuess) {
+      guessMessage.innerHTML = `⛔ Type a number between ${vars.minNumber} and ${vars.maxNumber}...`;
+    } else {
+      updateOnClick(userGuess, guessMessage);
+
+      if (vars.score == 0) {
+        outOfScore();
+        guessMessage.text("⛔ Try again! Changing number...");
+        vars.secretNumber = setSecretNumber(vars.minNumber, vars.maxNumber);
+      }
+    }
+  }
 };
+
+// Interval functions.
+setInterval(function () {
+  setValuesOnPage(vars.minNumber, vars.maxNumber, vars.score, vars.totalScore);
+  console.log(vars.score);
+}, 100);
 
 // Executing functions on the page.
 initialPageState();
--- scripts/script_old.js
+++ scripts/script_old.js
@@ -1,273 +1,273 @@
-"use strict";
+// "use strict";
 
-// **********************************************
-// Main page functionality, variables and updates
-// **********************************************
-// Shorthand for document ready with jQuery
-$(function () {
-  // Variables used throughout the game
-  const minNumber = 1;
-  let maxNumber = 10;
-  let score = Math.floor(maxNumber / 2);
-  let totalScore = 0;
-  let secretNumber = setSecretNumber(minNumber, maxNumber);
-  let isGuessed = false;
+// // **********************************************
+// // Main page functionality, variables and updates
+// // **********************************************
+// // Shorthand for document ready with jQuery
+// $(function () {
+//   // Variables used throughout the game
+//   const minNumber = 1;
+//   let maxNumber = 10;
+//   let score = Math.floor(maxNumber / 2);
+//   let totalScore = 0;
+//   let secretNumber = setSecretNumber(minNumber, maxNumber);
+//   let isGuessed = false;
 
-  // Display initial values.
-  // setValuesOnPage();
+//   // Display initial values.
+//   // setValuesOnPage();
 
-  // Functionality for the button Again! that appears after the user runs out of their current score or if they guess correctly.
-  // $(".btn.again").click(function () {
-  //   againClicked();
-  // });
+//   // Functionality for the button Again! that appears after the user runs out of their current score or if they guess correctly.
+//   // $(".btn.again").click(function () {
+//   //   againClicked();
+//   // });
 
-  // Functionality for the button Check! that is under the input field.
-  $(".btn.check").click(function () {
-    checkClicked();
-  });
+//   // Functionality for the button Check! that is under the input field.
+//   $(".btn.check").click(function () {
+//     checkClicked();
+//   });
 
-  // ***********************
-  // Page and game functions
-  // ***********************
-  function againClicked() {
-    score = Math.floor(maxNumber / 2);
-    resetScreen();
-  }
+//   // ***********************
+//   // Page and game functions
+//   // ***********************
+//   function againClicked() {
+//     score = Math.floor(maxNumber / 2);
+//     resetScreen();
+//   }
 
-  function checkClicked() {
-    let guess = $(".guess").val();
-    let guessMessage = $(".message");
+//   function checkClicked() {
+//     let guess = $(".guess").val();
+//     let guessMessage = $(".message");
 
-    // If the input field is empty the user is informed.
-    if (!guess) {
-      guessMessage.text("⛔ No number typed!");
-      // If the input field has a number, a updateOnClick is performed.
-      // Updates the score and total score on the screen.
-      // Generates a new secret number.
-    } else {
-      updateOnClick(guess, guessMessage);
+//     // If the input field is empty the user is informed.
+//     if (!guess) {
+//       guessMessage.text("⛔ No number typed!");
+//       // If the input field has a number, a updateOnClick is performed.
+//       // Updates the score and total score on the screen.
+//       // Generates a new secret number.
+//     } else {
+//       updateOnClick(guess, guessMessage);
 
-      if (score == 0) {
-        outOfScore();
-        guessMessage.text("⛔ Try again! Changing number...");
-        secretNumber = setSecretNumber();
-      }
-    }
-  }
+//       if (score == 0) {
+//         outOfScore();
+//         guessMessage.text("⛔ Try again! Changing number...");
+//         secretNumber = setSecretNumber();
+//       }
+//     }
+//   }
 
-  function setSecretNumber() {
-    return Math.floor(Math.random() * maxNumber) + minNumber;
-  }
+//   function setSecretNumber() {
+//     return Math.floor(Math.random() * maxNumber) + minNumber;
+//   }
 
-  function setBetweenRange() {
-    $(".between").text(`(Between ${minNumber} and ${maxNumber})`);
-  }
+//   function setBetweenRange() {
+//     $(".between").text(`(Between ${minNumber} and ${maxNumber})`);
+//   }
 
-  function setScore(score) {
-    $(".score").text(`${score}`);
-  }
+//   function setScore(score) {
+//     $(".score").text(`${score}`);
+//   }
 
-  function setTotalScore(totalScore) {
-    $(".total-score").text(`${totalScore}`);
-  }
+//   function setTotalScore(totalScore) {
+//     $(".total-score").text(`${totalScore}`);
+//   }
 
-  function setValuesOnPage() {
-    setBetweenRange(minNumber, maxNumber);
-    setScore(score);
-    setTotalScore(totalScore.toFixed(1));
-  }
+//   function setValuesOnPage() {
+//     setBetweenRange(minNumber, maxNumber);
+//     setScore(score);
+//     setTotalScore(totalScore.toFixed(1));
+//   }
 
-  function updateOnClick(guess, guessMessage) {
-    if (guess == secretNumber) {
-      guessedNumber(secretNumber);
-      guessMessage.html("✅ You guessed the number! Changing number...");
-      totalScore += score;
-      secretNumber = setSecretNumber();
-      isGuessed = true;
-    } else if (guess > secretNumber) {
-      guessMessage.html("☝ Too high...");
-      score--;
-    } else if (guess < secretNumber) {
-      guessMessage.html("👇 Too low...");
-      score--;
-    }
-  }
+//   function updateOnClick(guess, guessMessage) {
+//     if (guess == secretNumber) {
+//       guessedNumber(secretNumber);
+//       guessMessage.html("✅ You guessed the number! Changing number...");
+//       totalScore += score;
+//       secretNumber = setSecretNumber();
+//       isGuessed = true;
+//     } else if (guess > secretNumber) {
+//       guessMessage.html("☝ Too high...");
+//       score--;
+//     } else if (guess < secretNumber) {
+//       guessMessage.html("👇 Too low...");
+//       score--;
+//     }
+//   }
 
-  function guessedNumber(secretNumber) {
-    $("body").css({ "background-color": "#60b347" });
-    $(".number").css({ width: "30rem" });
-    $(".btn.check").css({ display: "none" });
-    $(".btn.again").css({ display: "inline-block" });
-    $(".number").text(`${secretNumber}`);
-  }
+//   function guessedNumber(secretNumber) {
+//     $("body").css({ "background-color": "#60b347" });
+//     $(".number").css({ width: "30rem" });
+//     $(".btn.check").css({ display: "none" });
+//     $(".btn.again").css({ display: "inline-block" });
+//     $(".number").text(`${secretNumber}`);
+//   }
 
-  function outOfScore() {
-    $("body").css({ "background-color": "#ab263a" });
-    $(".btn.check").css({ display: "none" });
-    $(".btn.again").css({ display: "inline-block" });
-    $(".number").text("X");
-  }
+//   function outOfScore() {
+//     $("body").css({ "background-color": "#ab263a" });
+//     $(".btn.check").css({ display: "none" });
+//     $(".btn.again").css({ display: "inline-block" });
+//     $(".number").text("X");
+//   }
 
-  function resetScreen() {
-    isGuessed = false;
-    $("body").css({ "background-color": "#222" });
-    $(".number").css({ width: "15rem" });
-    $(".btn.again").css({ display: "none" });
-    $(".btn.check").css({ display: "inline-block" });
-    $(".number").text("?");
-    $(".message").html("Start guessing...");
-  }
+//   function resetScreen() {
+//     isGuessed = false;
+//     $("body").css({ "background-color": "#222" });
+//     $(".number").css({ width: "15rem" });
+//     $(".btn.again").css({ display: "none" });
+//     $(".btn.check").css({ display: "inline-block" });
+//     $(".number").text("?");
+//     $(".message").html("Start guessing...");
+//   }
 
-  $("input").bind("enterKey", function (e) {
-    if (score > 0 && !isGuessed) {
-      checkClicked();
-    } else {
-      againClicked();
-    }
-  });
-  $("input").keyup(function (e) {
-    if (e.keyCode == 13) {
-      $(this).trigger("enterKey");
-    }
-  });
+//   $("input").bind("enterKey", function (e) {
+//     if (score > 0 && !isGuessed) {
+//       checkClicked();
+//     } else {
+//       againClicked();
+//     }
+//   });
+//   $("input").keyup(function (e) {
+//     if (e.keyCode == 13) {
+//       $(this).trigger("enterKey");
+//     }
+//   });
 
-  // ******************************************************
-  // Upgrade tab functionality - SPU means Score Per Update
-  // ******************************************************
+//   // ******************************************************
+//   // Upgrade tab functionality - SPU means Score Per Update
+//   // ******************************************************
 
-  // Need to make the items into classes and use OOP
-  $(".btn.upgrade").click(function () {
-    toggleUpgradeTab();
-  });
+//   // Need to make the items into classes and use OOP
+//   $(".btn.upgrade").click(function () {
+//     toggleUpgradeTab();
+//   });
 
-  $(".overlay").click(function () {
-    toggleUpgradeTab();
-  });
+//   $(".overlay").click(function () {
+//     toggleUpgradeTab();
+//   });
 
-  $(".close-upgrade").click(function () {
-    toggleUpgradeTab();
-  });
+//   $(".close-upgrade").click(function () {
+//     toggleUpgradeTab();
+//   });
 
-  function updateUpgradeTabCurrency() {
-    $(".currency").text(totalScore.toFixed(1));
-  }
+//   function updateUpgradeTabCurrency() {
+//     $(".currency").text(totalScore.toFixed(1));
+//   }
 
-  function toggleUpgradeTab() {
-    $(".upgrade-tab").toggleClass("hidden");
-    $(".overlay").toggleClass("hidden");
-    updateUpgradeTabCurrency();
-  }
+//   function toggleUpgradeTab() {
+//     $(".upgrade-tab").toggleClass("hidden");
+//     $(".overlay").toggleClass("hidden");
+//     updateUpgradeTabCurrency();
+//   }
 
-  let simpleAlgorithmSPU = 0.001;
-  let simpleAlgorithmCount = 0;
+//   let simpleAlgorithmSPU = 0.001;
+//   let simpleAlgorithmCount = 0;
 
-  let simpleAlgorithmBuyPrice = 10;
-  const simpleAlgorithmBuyPriceIncrease = 10;
+//   let simpleAlgorithmBuyPrice = 10;
+//   const simpleAlgorithmBuyPriceIncrease = 10;
 
-  let simpleAlgorithmUpgradePrice = 15;
-  const simpleAlgorithmUpgradePriceIncrease = 15;
+//   let simpleAlgorithmUpgradePrice = 15;
+//   const simpleAlgorithmUpgradePriceIncrease = 15;
 
-  $(".simple-algorithm button.buy-item").click(function () {
-    buySimpleAlgorithm();
-  });
+//   $(".simple-algorithm button.buy-item").click(function () {
+//     buySimpleAlgorithm();
+//   });
 
-  $(".simple-algorithm button.upgrade-it").click(function () {
-    upgradeSimpleAlgorithm();
-  });
+//   $(".simple-algorithm button.upgrade-it").click(function () {
+//     upgradeSimpleAlgorithm();
+//   });
 
-  $(".simple-algorithm button.sell-item").click(function () {
-    sellSimpleAlgorithm();
-  });
+//   $(".simple-algorithm button.sell-item").click(function () {
+//     sellSimpleAlgorithm();
+//   });
 
-  function buySimpleAlgorithm() {
-    if (totalScore >= simpleAlgorithmBuyPrice) {
-      totalScore -= simpleAlgorithmBuyPrice;
-      simpleAlgorithmCount++;
-      simpleAlgorithmBuyPrice =
-        simpleAlgorithmCount * simpleAlgorithmBuyPriceIncrease +
-        simpleAlgorithmBuyPriceIncrease;
-      updateSimpleAlgorithmInfo();
-    } else {
-      $(".simple-algorithm button.buy-item").shake();
-    }
-  }
+//   function buySimpleAlgorithm() {
+//     if (totalScore >= simpleAlgorithmBuyPrice) {
+//       totalScore -= simpleAlgorithmBuyPrice;
+//       simpleAlgorithmCount++;
+//       simpleAlgorithmBuyPrice =
+//         simpleAlgorithmCount * simpleAlgorithmBuyPriceIncrease +
+//         simpleAlgorithmBuyPriceIncrease;
+//       updateSimpleAlgorithmInfo();
+//     } else {
+//       $(".simple-algorithm button.buy-item").shake();
+//     }
+//   }
 
-  function upgradeSimpleAlgorithm() {
-    if (totalScore >= simpleAlgorithmUpgradePrice && simpleAlgorithmCount > 0) {
-      totalScore -= simpleAlgorithmUpgradePrice;
-      simpleAlgorithmSPU += 0.01;
-      simpleAlgorithmUpgradePrice += simpleAlgorithmUpgradePriceIncrease;
-      updateSimpleAlgorithmInfo();
-    } else {
-      $(".simple-algorithm button.upgrade-it").shake();
-    }
-  }
+//   function upgradeSimpleAlgorithm() {
+//     if (totalScore >= simpleAlgorithmUpgradePrice && simpleAlgorithmCount > 0) {
+//       totalScore -= simpleAlgorithmUpgradePrice;
+//       simpleAlgorithmSPU += 0.01;
+//       simpleAlgorithmUpgradePrice += simpleAlgorithmUpgradePriceIncrease;
+//       updateSimpleAlgorithmInfo();
+//     } else {
+//       $(".simple-algorithm button.upgrade-it").shake();
+//     }
+//   }
 
-  function sellSimpleAlgorithm() {
-    if (simpleAlgorithmCount > 0) {
-      totalScore += simpleAlgorithmBuyPrice - simpleAlgorithmBuyPriceIncrease;
-      simpleAlgorithmCount--;
-      simpleAlgorithmBuyPrice =
-        simpleAlgorithmCount * simpleAlgorithmBuyPriceIncrease +
-        simpleAlgorithmBuyPriceIncrease;
-      updateSimpleAlgorithmInfo();
-    } else {
-      $(".simple-algorithm button.sell-item").shake();
-    }
-  }
+//   function sellSimpleAlgorithm() {
+//     if (simpleAlgorithmCount > 0) {
+//       totalScore += simpleAlgorithmBuyPrice - simpleAlgorithmBuyPriceIncrease;
+//       simpleAlgorithmCount--;
+//       simpleAlgorithmBuyPrice =
+//         simpleAlgorithmCount * simpleAlgorithmBuyPriceIncrease +
+//         simpleAlgorithmBuyPriceIncrease;
+//       updateSimpleAlgorithmInfo();
+//     } else {
+//       $(".simple-algorithm button.sell-item").shake();
+//     }
+//   }
 
-  function updateSimpleAlgorithmInfo() {
-    $(".simple-algorithm span.count").text(simpleAlgorithmCount);
-    $(".simple-algorithm button.price").text(simpleAlgorithmBuyPrice);
-    $(".simple-algorithm .buy-price").text(simpleAlgorithmBuyPrice);
-    $(".simple-algorithm .upgrade-price").text(simpleAlgorithmUpgradePrice);
-    $(".simple-algorithm .sell-price").text(
-      simpleAlgorithmBuyPrice - simpleAlgorithmBuyPriceIncrease
-    );
-    $(".simple-algorithm .current-sps").text(
-      (simpleAlgorithmSPU * simpleAlgorithmCount * 10).toFixed(1)
-    );
-  }
+//   function updateSimpleAlgorithmInfo() {
+//     $(".simple-algorithm span.count").text(simpleAlgorithmCount);
+//     $(".simple-algorithm button.price").text(simpleAlgorithmBuyPrice);
+//     $(".simple-algorithm .buy-price").text(simpleAlgorithmBuyPrice);
+//     $(".simple-algorithm .upgrade-price").text(simpleAlgorithmUpgradePrice);
+//     $(".simple-algorithm .sell-price").text(
+//       simpleAlgorithmBuyPrice - simpleAlgorithmBuyPriceIncrease
+//     );
+//     $(".simple-algorithm .current-sps").text(
+//       (simpleAlgorithmSPU * simpleAlgorithmCount * 10).toFixed(1)
+//     );
+//   }
 
-  function totalScoreIncreaseSimpleAlgorithm() {
-    totalScore += Number(simpleAlgorithmCount * simpleAlgorithmSPU);
-  }
+//   function totalScoreIncreaseSimpleAlgorithm() {
+//     totalScore += Number(simpleAlgorithmCount * simpleAlgorithmSPU);
+//   }
 
-  // *********
-  // Intervals
-  // *********
+//   // *********
+//   // Intervals
+//   // *********
 
-  setInterval(function () {
-    totalScoreIncreaseSimpleAlgorithm();
-    setValuesOnPage();
-    updateUpgradeTabCurrency();
-  }, 100);
-});
+//   setInterval(function () {
+//     totalScoreIncreaseSimpleAlgorithm();
+//     setValuesOnPage();
+//     updateUpgradeTabCurrency();
+//   }, 100);
+// });
 
-// ****************
-// jQuery Functions
-// ****************
+// // ****************
+// // jQuery Functions
+// // ****************
 
-$.fn.shake = function (interval = 100) {
-  this.addClass("shaking");
-  this.css("transition", "all " + (interval / 100).toString() + "s");
-  setTimeout(() => {
-    this.css("transform", "translateX(-50%)");
-  }, interval * 0);
-  setTimeout(() => {
-    this.css("transform", "translateX(50%)");
-  }, interval * 1);
-  setTimeout(() => {
-    this.css("transform", "translateX(-25%)");
-  }, interval * 2);
-  setTimeout(() => {
-    this.css("transform", "translateX(25%)");
-  }, interval * 3);
-  setTimeout(() => {
-    this.css("transform", "translateX(-7%)");
-  }, interval * 4);
-  setTimeout(() => {
-    this.css("transform", "translateX(0%)");
-  }, interval * 5);
-  this.removeClass("shaking");
-};
+// $.fn.shake = function (interval = 100) {
+//   this.addClass("shaking");
+//   this.css("transition", "all " + (interval / 100).toString() + "s");
+//   setTimeout(() => {
+//     this.css("transform", "translateX(-50%)");
+//   }, interval * 0);
+//   setTimeout(() => {
+//     this.css("transform", "translateX(50%)");
+//   }, interval * 1);
+//   setTimeout(() => {
+//     this.css("transform", "translateX(-25%)");
+//   }, interval * 2);
+//   setTimeout(() => {
+//     this.css("transform", "translateX(25%)");
+//   }, interval * 3);
+//   setTimeout(() => {
+//     this.css("transform", "translateX(-7%)");
+//   }, interval * 4);
+//   setTimeout(() => {
+//     this.css("transform", "translateX(0%)");
+//   }, interval * 5);
+//   this.removeClass("shaking");
+// };
