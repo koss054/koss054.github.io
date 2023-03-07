7b6f8a0c977dd72af265f901563e838c16880735
koss054 <konstantin054@abv.bg>
2023-03-07 16:21:03 +0200

    Create Page class and move page functions inside

--- scripts/script.js
+++ scripts/script.js
@@ -15,127 +15,131 @@ class Player {
   resetScore() {
     this.currentScore = Math.ceil(this.maxNumber / 2);
   }
-}
 
-// Functions used in page functions.
-function setValuesOnPage(player) {
-  setBetweenRange(player.minNumber, player.maxNumber);
-  setScore(player.currentScore);
-  setTotalScore(player.totalScore);
-}
-
-function setBetweenRange(minNumber, maxNumber) {
-  document.getElementById(
-    "between"
-  ).innerHTML = `(Between ${minNumber} and ${maxNumber})`;
-}
-
-function setScore(currentScore) {
-  document.getElementById("score").innerHTML = `${currentScore}`;
-}
-
-function setTotalScore(totalScore) {
-  document.getElementById("total-score").innerHTML = `${totalScore.toFixed(1)}`;
-}
-
-function setSecretNumber(player) {
-  return Math.floor(Math.random() * player.maxNumber) + player.minNumber;
-}
-
-function resetScreen(player) {
-  player.isGuessed = false;
-  document.querySelector("body").style.background = "#222";
-  document.querySelector(".number").style.width = "15rem";
-  document.querySelector(".btn.again").style.display = "none";
-  document.querySelector(".btn.check").style.display = "inline-block";
-  document.querySelector(".number").innerHTML = "?";
-  document.querySelector("#message").innerHTML = "Start guessing...";
-}
-
-function updateOnClick(player, playerGuess, guessMessageElement) {
-  console.log(player);
-  if (playerGuess == player.secretNumber) {
-    guessedNumber(player.secretNumber);
-    guessMessageElement.innerHTML =
-      "✅ You guessed the number! Changing number...";
-    player.totalScore += player.currentScore;
-    player.secretNumber = setSecretNumber(player);
-    player.isGuessed = true;
-  } else if (playerGuess > player.secretNumber) {
-    guessMessageElement.innerHTML = "☝ Too high...";
-    player.currentScore--;
-  } else if (playerGuess < player.secretNumber) {
-    guessMessageElement.innerHTML = "👇 Too low...";
-    player.currentScore--;
+  setSecretNumber() {
+    this.secretNumber =
+      Math.floor(Math.random() * this.maxNumber) + this.minNumber;
   }
 }
 
-function guessedNumber(secretNumber) {
-  document.querySelector("body").style.background = "#60b347";
-  document.querySelector(".number").style.width = "30rem";
-  document.querySelector(".btn.check").style.display = "none";
-  document.querySelector(".btn.again").style.display = "inline-block";
-  document.querySelector(".number").innerHTML = `${secretNumber}`;
+// Functions used in page functions.
+class Page {
+  // Private methods.
+  #setBetweenRange(minNumber, maxNumber) {
+    document.getElementById(
+      "between"
+    ).innerHTML = `(Between ${minNumber} and ${maxNumber})`;
+  }
+
+  #setScore(currentScore) {
+    document.getElementById("score").innerHTML = `${currentScore}`;
+  }
+
+  #setTotalScore(totalScore) {
+    document.getElementById("total-score").innerHTML = `${totalScore.toFixed(
+      1
+    )}`;
+  }
+
+  // Public methods.
+  setValuesOnPage(player) {
+    this.#setBetweenRange(player.minNumber, player.maxNumber);
+    this.#setScore(player.currentScore);
+    this.#setTotalScore(player.totalScore);
+  }
+
+  resetScreen(player) {
+    player.isGuessed = false;
+    document.querySelector("body").style.background = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".btn.again").style.display = "none";
+    document.querySelector(".btn.check").style.display = "inline-block";
+    document.querySelector(".number").innerHTML = "?";
+    document.querySelector("#message").innerHTML = "Start guessing...";
+  }
+
+  guessedNumber(secretNumber) {
+    document.querySelector("body").style.background = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".btn.check").style.display = "none";
+    document.querySelector(".btn.again").style.display = "inline-block";
+    document.querySelector(".number").innerHTML = `${secretNumber}`;
+  }
+
+  updateOnClick(player, playerGuess, guessMessageElement) {
+    console.log(player);
+    if (playerGuess == player.secretNumber) {
+      this.guessedNumber(player.secretNumber);
+      guessMessageElement.innerHTML =
+        "✅ You guessed the number! Changing number...";
+      player.totalScore += player.currentScore;
+      player.setSecretNumber();
+      player.isGuessed = true;
+    } else if (playerGuess > player.secretNumber) {
+      guessMessageElement.innerHTML = "☝ Too high...";
+      player.currentScore--;
+    } else if (playerGuess < player.secretNumber) {
+      guessMessageElement.innerHTML = "👇 Too low...";
+      player.currentScore--;
+    }
+  }
+
+  outOfScore() {
+    document.querySelector("body").style.background = "#ab263a";
+    document.querySelector(".btn.check").style.display = "none";
+    document.querySelector(".btn.again").style.display = "inline-block";
+    document.querySelector(".number").innerHTML = "X";
+  }
 }
 
-function outOfScore() {
-  document.querySelector("body").style.background = "#ab263a";
-  document.querySelector(".btn.check").style.display = "none";
-  document.querySelector(".btn.again").style.display = "inline-block";
-  document.querySelector(".number").innerHTML = "X";
-}
-
-// Page functions.
-const initialPageState = function (player) {
-  // Function that sets all initial values.
-  setValuesOnPage(player);
-};
-
-const buttonEvents = function (player) {
+// Button events function.
+const buttonEvents = function (player, page) {
   // Button elements.
   const btnAgain = document.querySelector(".btn.again");
   const btnCheck = document.querySelector(".btn.check");
 
   // Button events.
   btnAgain.addEventListener("click", function () {
-    againClicked(player);
+    againClicked(player, page);
   });
 
   btnCheck.addEventListener("click", function () {
-    checkClicked(player);
+    checkClicked(player, page);
   });
 
   // Inner functions for buttons.
-  function againClicked(player) {
+  function againClicked(player, page) {
     player.currentScore = Math.ceil(player.maxNumber / 2);
-    resetScreen(player);
+    page.resetScreen(player, page);
   }
 
-  function checkClicked(player) {
+  function checkClicked(player, page) {
     const userGuess = document.getElementById("guess").value;
     const guessMessage = document.getElementById("message");
 
     if (!userGuess) {
       guessMessage.innerHTML = `⛔ Type a number between ${player.minNumber} and ${player.maxNumber}...`;
     } else {
-      updateOnClick(player, userGuess, guessMessage);
+      page.updateOnClick(player, userGuess, guessMessage);
 
       if (player.currentScore == 0) {
-        outOfScore();
-        guessMessage.text("⛔ Try again! Changing number...");
-        player.secretNumber = setSecretNumber(player);
+        page.outOfScore();
+        guessMessage.innerHTML =
+          "⛔ You didn't guess! Changing secret number...";
+        player.setSecretNumber();
       }
     }
   }
 };
 
-// Executing functions on the page.
+// Initializing classes.
 const player = new Player();
+const page = new Page();
 
-initialPageState(player);
-buttonEvents(player);
+// Page functions.
+buttonEvents(player, page);
 
-// Interval functions.
+// Interval function(s).
 setInterval(function () {
-  setValuesOnPage(player);
+  page.setValuesOnPage(player);
 }, 100);
