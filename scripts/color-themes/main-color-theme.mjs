e80be8c952d29d9975b529c9dce9aaf9121ae782
koss054 <konstantin054@abv.bg>
2023-03-15 14:04:30 +0200

    Create initial main color theme class

---
+++ scripts/color-themes/main-color-theme.mjs
@@ -0,0 +1,14 @@
+export class MainColorTheme {
+  constructor() {
+    this.accentColor = "#eee";
+    this.buttonHoverColor = "#ccc";
+  }
+
+  getAccentColor() {
+    return this.accentColor;
+  }
+
+  getButtonHoverColor() {
+    return this.buttonHoverColor;
+  }
+}
