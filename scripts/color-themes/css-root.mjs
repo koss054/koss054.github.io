ecc49b1799fd3cee72dbbddc987ede90ed91a7d8
koss054 <konstantin054@abv.bg>
2023-03-15 14:17:57 +0200

    Create initial root class

---
+++ scripts/color-themes/css-root.mjs
@@ -0,0 +1,33 @@
+export class Root {
+  #varMainDarkColor = "--main-dark-color";
+  #varGuessedBgColor = "--guessed-background-color";
+  #varFailedBgColor = "--failed-background-color";
+
+  #bodyElement = document.querySelector("body");
+
+  constructor() {
+    this.root = document.querySelector(":root");
+    this.rootStyle = getComputedStyle(this.root);
+  }
+
+  showInitialBackgroundColor() {
+    const initialBgColor = this.rootStyle.getPropertyValue(
+      this.#varMainDarkColor
+    );
+    this.#bodyElement.style.background = initialBgColor;
+  }
+
+  showGuessedBackgroundColor() {
+    const guessedBgColor = this.rootStyle.getPropertyValue(
+      this.#varGuessedBgColor
+    );
+    this.#bodyElement.style.background = guessedBgColor;
+  }
+
+  showFaileddBackgroundColor() {
+    const faiedBgColor = this.rootStyle.getPropertyValue(
+      this.#varFailedBgColor
+    );
+    this.#bodyElement.style.background = faiedBgColor;
+  }
+}
